d98c35829cce9507627d0620959b93680b1a9f9a
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-11-06 12:30:44 +0300

    style: form styling

--- hng-app/src/Tag.js
+++ hng-app/src/Tag.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Tag(){
     return(
-<a id= "contact" href="/contact"> <p className= "contactme">Contact Me</p></a>
+<a id= "contact" href="/contact"> <p className="contactme">Contact Me</p></a>
     )
 }
 export default Tag;
